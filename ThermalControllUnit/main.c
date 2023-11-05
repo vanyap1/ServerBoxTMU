e184f6d610fae8deffc991973c45331e9e572c12
Ivan <princ.va@gmail.com>
2023-11-05 22:50:54 +0200

    interrupt and pwm controller

--- ThermalControllUnit/main.c
+++ ThermalControllUnit/main.c
@@ -41,7 +41,7 @@ gpio out6 = {(uint8_t *)&PORTC , PORTC5};
 gpio out7 = {(uint8_t *)&PORTC , PORTC6};
 gpio out8 = {(uint8_t *)&PORTC , PORTC7};
 
-
+gpio fan3_pe5 = {(uint8_t *)&PORTE , PORTE5};
 	
 rtc_date sys_rtc = {
 	.date = 27,
@@ -53,13 +53,30 @@ rtc_date sys_rtc = {
 	.second = 00
 };	
 	
+uint8_t timerIRQ = 0;
+uint16_t tempZone1 = 0;
+uint16_t tempZone2 = 0;
+uint16_t tempZone3 = 0;
+	
+	
+#define	MAX_TEMP	300	
+#define	MIN_PWM		25
+#define MAX_PWM		250
+
+	
 /*
 Next - 
 ADC configuration					<< DONE
 ADC value to Temperature			<< DONE
-PWM
+PWM									<< DONE
 FAN control logic (Perhaps - PID)
+
 */
+	
+ISR(TIMER1_COMPA_vect){
+	timerIRQ = 1;
+}
+
 	
 int main(void)
 {
@@ -74,7 +91,7 @@ int main(void)
     gpio_set_pin_direction(&ld1 , PORT_DIR_OUT); gpio_set_pin_level(&ld1, true);
     gpio_set_pin_direction(&ld2 , PORT_DIR_OUT); gpio_set_pin_level(&ld2, true);
 	gpio_set_pin_direction(&ioEn , PORT_DIR_OUT); gpio_set_pin_level(&ioEn, true);
-    
+    gpio_set_pin_direction(&fan3_pe5 , PORT_DIR_OUT);
 	
 	sei();
 	stdout = &mystdout;
@@ -83,18 +100,47 @@ int main(void)
 	adc_init();
 	spi_init();
 	
+	//TIMER3 PWM init
+	#define  FAN3	OCR3C	
+	TCCR3A |= (0b10 << COM3C0) | (0b01 << WGM10);
+	TCCR3B = (0b00 << WGM12) | (0b010 << CS10); 
+	TCCR3C = (1 << FOC3C);
+	FAN3 = 40;
+	//Write data to OCR3C
+	
+	
+	
+	
+	
+	//Interrupt controller 
+	TIMSK |= (1 << OCIE1A);
+	TCCR1A |= (0b10 << WGM00);
+	OCR1A = 50; // TOP value
+	TCCR1B |= (0b011<< CS00);
+	//
+	
 	
     while (1) 
     {
-		gpio_set_pin_level(&out3, true);
-		_delay_ms(500);
-		gpio_set_pin_level(&out3, false);
-		_delay_ms(500);
-		gpio_toggle_pin_level(&out1);
+		
+		if(timerIRQ){
+			timerIRQ = 0;
+			gpio_toggle_pin_level(&out1);
+			tempZone2 = getNTC(NTC2);
+			if(tempZone2 > MAX_TEMP){
+				FAN3 < MAX_PWM ? FAN3++ : NULL;
+			}else{
+				FAN3 > MIN_PWM ? FAN3-- : NULL;
+			}
+			
+			
+			rtc_sync(&sys_rtc);
+			printf("Time: %02d-%02d-20%02d; %02d:%02d:%02d; NTC: %03d; FAN:  %03d\r\n", sys_rtc.date, sys_rtc.month, sys_rtc.year, sys_rtc.hour, sys_rtc.minute, sys_rtc.second, tempZone2, FAN3);
+			
+		}
 		
 		
-		rtc_sync(&sys_rtc);
-		printf("Time: %02d-%02d-20%02d; %02d:%02d:%02d; NTC: %03d\r\n", sys_rtc.date, sys_rtc.month, sys_rtc.year, sys_rtc.hour, sys_rtc.minute, sys_rtc.second, getNTC(NTC2));
+		
 		
     }
 }
